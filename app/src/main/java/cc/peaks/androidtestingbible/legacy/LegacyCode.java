4eb5f27e098bf4cf89fa3bc9275a082a48ad7ea5
aha-oretama <sekine_y_529@msn.com>
2019-05-09 14:43:00 +0900

    Add 05.

--- app/src/main/java/cc/peaks/androidtestingbible/legacy/LegacyCode.java
+++ app/src/main/java/cc/peaks/androidtestingbible/legacy/LegacyCode.java
@@ -4,15 +4,11 @@ import android.content.Context;
 import android.support.annotation.VisibleForTesting;
 
 public class LegacyCode {
-    @VisibleForTesting
-    LocalDataFetcher localDataFetcher = new LocalDataFetcher();
-    @VisibleForTesting
-    RemoteDataFetcher remoteDataFetcher = new RemoteDataFetcher();
-    @VisibleForTesting
-    NetworkUtilsWrapper networkUtils = new NetworkUtilsWrapper();
+    private LocalDataFetcher localDataFetcher = new LocalDataFetcher();
+    private RemoteDataFetcher remoteDataFetcher = new RemoteDataFetcher();
+    private NetworkUtilsWrapper networkUtils = new NetworkUtilsWrapper();
     private DataConverter dataConverter = new DataConverter();
-
-
+    
     void loadData(String param, Context context, Callback<NewData> callback) {
         OldData oldData = load(param, context);
         NewData newData = dataConverter.convert(oldData);
--- app/src/test/java/cc/peaks/androidtestingbible/legacy/LegacyCodeTest.kt
+++ app/src/test/java/cc/peaks/androidtestingbible/legacy/LegacyCodeTest.kt
@@ -1,53 +1,63 @@
 package cc.peaks.androidtestingbible.legacy
 
-import android.content.Context
 import androidx.test.InstrumentationRegistry
-import com.nhaarman.mockitokotlin2.anyOrNull
+import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.argumentCaptor
 import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.spy
 import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.mockito.Spy
 import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
 class LegacyCodeTest {
-  lateinit var legacyCode: LegacyCode
-  private lateinit var callback: LegacyCode.Callback<NewData>
+    private lateinit var callback: LegacyCode.Callback<NewData>
 
-  @Before
-  fun setUp() {
-      legacyCode = LegacyCode()
-      legacyCode.localDataFetcher = object : LocalDataFetcher() {
-          override fun fetch(param: String?): OldData {
-              return OldData("local:$param")
-          }
-      }
-      legacyCode.remoteDataFetcher = object : RemoteDataFetcher() {
-          override fun fetch(param: String?): OldData {
-              return OldData("remote:$param")
-          }
-      }
-      legacyCode.networkUtils = object : NetworkUtilsWrapper() {
-          override fun isOnline(context: Context?): Boolean {
-              return true
-          }
-      }
-      callback = mock()
-  }
+    @Mock(name = "localDataFetcher")
+    private lateinit var localDataFetcher: LocalDataFetcher
+    @Mock(name = "remoteDataFetcher")
+    private lateinit var remoteDataFetcher: RemoteDataFetcher
+    @Mock(name = "networkUtils")
+    private lateinit var networkUtilsWrapper: NetworkUtilsWrapper
+    @Spy
+    val converter = DataConverter()
 
-  @Test
-  fun loadData() {
-      val context = InstrumentationRegistry.getTargetContext()
-      legacyCode.loadData("foo", context, callback)
+    @InjectMocks
+    lateinit var legacyCode: LegacyCode
 
-      argumentCaptor<NewData>().apply {
-          verify(callback, times(1)).onSuccess(capture())
-          assertThat(firstValue.data).isEqualTo("converted:remote:foo")
-      }
-  }
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        whenever(localDataFetcher.fetch(any())).thenAnswer { invocation ->
+            val param = invocation.arguments[0] as String
+            return@thenAnswer OldData("local:$param")
+        }
+        whenever(remoteDataFetcher.fetch(any())).thenAnswer { invocation ->
+            val param = invocation.arguments[0] as String
+            return@thenAnswer OldData("remote:$param")
+        }
+        whenever(networkUtilsWrapper.isOnline(any())).thenReturn(true)
+
+        callback = mock()
+    }
+
+    @Test
+    fun loadData() {
+        val context = InstrumentationRegistry.getTargetContext()
+        legacyCode.loadData("foo", context, callback)
+
+        argumentCaptor<NewData>().apply {
+            verify(callback, times(1)).onSuccess(capture())
+            verify(converter).convert(any())
+            assertThat(firstValue.data).isEqualTo("converted:remote:foo")
+        }
+    }
 }
